1800fbc7c926d5ff589ba9e5c7cf76a9c7b09d41
Christian <bournecreative@gmail.com>
2024-02-29 23:22:20 -0600

    initialize nextjs repo

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+
+export default function About() {
+  return (
+    <main>
+      <div>
+        <p>
+          Get started by editing&nbsp;
+          <code>src/app/page.tsx</code>
+        </p>
+    </div>
+    </main>
+    
+  );
+}
