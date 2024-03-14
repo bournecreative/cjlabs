041e4e9f7dd5ce475a0910c611cd74a8dfdd6a79
Christian <bournecreative@gmail.com>
2024-03-14 01:03:40 -0500

    update Navigation to use new button component with prop instead of non composable version

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,13 @@
 import styles from './navigation.module.scss'
 import { SiteLogo } from '../SiteLogo/SiteLogo'
-import { NavigationButton } from '../NavigationButton/NavigationButton'
+import { Button } from '../Button/Button'
 
 export const Navigation = () => {
     return (
           <div className={styles.nav}>
             <SiteLogo />
             <div className={styles['nav__menu']}>
-               <NavigationButton />
+               <Button type="menu" />
             </div>
         </div>
     )
